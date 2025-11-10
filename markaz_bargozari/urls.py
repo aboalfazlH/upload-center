e68918e4ac4d14858b8edb15d5f1fb9f8b9cd2b6
saboalfazl6162 <saboalfazl6162@gmail.com>
2025-11-10 15:33:56 +0330

    add front 1.1

--- markaz_bargozari/urls.py
+++ markaz_bargozari/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard/',include('apps.upload_admin.urls')),
 ]
 
 if settings.DEBUG:
